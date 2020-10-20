611eeefe1a7336e9ddb3b1f5e6d99e02aafa2336
Luciano Pereyra <luciano.pereyra@gmail.com>
2020-10-19 22:46:59 -0300

    Maqueto la home en Desktop

--- scripts/script.js
+++ scripts/script.js
@@ -35,11 +35,12 @@ var observer = new IntersectionObserver(function(entries) {
     // no intersection with screen
     if (entries[0].intersectionRatio === 0)
     // document.querySelector("#nav-container").classList.add("nav-container-sticky");
-        alert("es sticky");
+    // alert("es sticky");
+        header.classList.add("headerSticky");
     // fully intersects with screen
     else if (entries[0].intersectionRatio === 1)
     // document.querySelector("#nav-container").classList.remove("nav-container-sticky");
-        alert("Ahora no debe sticky");
+        header.classList.remove("headerSticky");
 }, { threshold: [0, 1] });
 
 observer.observe(document.querySelector("#header-top"));
